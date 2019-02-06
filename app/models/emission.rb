282d42f413855eb9e0eff0e45942a86a2abc13cc
Peter Baker <pcbaker@gmail.com>
2019-02-06 14:58:04 +0100

    Move queries in test into Emission model.

--- app/models/emission.rb
+++ app/models/emission.rb
@@ -7,6 +7,15 @@ class Emission < ApplicationRecord
 
   validate :conformity_of_entry
 
+  class << self
+    def filter(territory_code)
+      world = Territory.where(code: territory_code).first
+      from_world = world.emissions
+      from_world_in_1850 = from_world.select { |emission| emission.period.year == 1850 }
+      results = from_world_in_1850.inject(0){|running_total, emission| running_total += emission.retrieve_tonnage }
+    end
+  end
+
   def retrieve_tonnage
     if credit.present?
       credit
--- test/integration/interface_to_filterable_charts_test.rb
+++ test/integration/interface_to_filterable_charts_test.rb
@@ -2,10 +2,7 @@ require 'test_helper'
 
 class InterfaceToFilterableChartsTest < ActionDispatch::IntegrationTest
   test "Emissions can be filtered by territories such as the whole world." do
-    world = territories(:world)
-    from_world = world.emissions
-    from_world_in_1850 = from_world.select { |emission| emission.period.year == 1850 }
-    results = from_world_in_1850.inject(0){|running_total, emission| running_total += emission.retrieve_tonnage }
+    results = Emission.filter('WORLD')
     assert(results == 2050.0)
   end
 end
