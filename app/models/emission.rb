17d34017e1ff6a1bffe8b917a41f45146fcd5f66
Peter Baker <pcbaker@gmail.com>
2019-02-06 15:00:11 +0100

    Rename local variables.

--- app/models/emission.rb
+++ app/models/emission.rb
@@ -9,10 +9,10 @@ class Emission < ApplicationRecord
 
   class << self
     def filter(territory_code)
-      world = Territory.where(code: territory_code).first
-      from_world = world.emissions
-      from_world_in_1850 = from_world.select { |emission| emission.period.year == 1850 }
-      results = from_world_in_1850.inject(0){|running_total, emission| running_total += emission.retrieve_tonnage }
+      territory = Territory.where(code: territory_code).first
+      from_territory = territory.emissions
+      from_territory_in_1850 = from_territory.select { |emission| emission.period.year == 1850 }
+      results = from_territory_in_1850.inject(0){|running_total, emission| running_total += emission.retrieve_tonnage }
     end
   end
 
